eb60bf6f422b7f5141d67b98acb2700f472301d1
Álvaro Barbero Jiménez <albarjip@gmail.com>
2020-09-19 13:34:48 +0200

    Updated to keras 2
    Removed no longer needed libraries

--- tests.py
+++ tests.py
@@ -5,22 +5,14 @@ import keras
 import matplotlib
 import sklearn
 import pandas
-import requests
 import scipy
 import numpy
-
-from eli5 import explain_prediction, format_as_image
 
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers.core import Activation
 from keras.layers.core import Dense
 from keras.utils import np_utils
-
-from skimage.io import imread
-from skimage.transform import resize
-
-from sklearn.model_selection import train_test_split
 
 import torch
 
@@ -37,10 +29,3 @@ def test_keras():
     model.summary()
     model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
     model.fit(trainvectors, Y_train, epochs=1)
-
-
-
-def test_requests():
-    """Tries to perform a request to a public server"""
-    r = requests.get('http://www.google.com/search?q=deep+learning')
-    print(r.text)
